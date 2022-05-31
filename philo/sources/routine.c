13466f9866bb08fe2f607f8f4f5d1a92a9e6a797
Francisco Purdom <fpurdom@f1r4s11.codam.nl>
2022-05-31 20:04:13 +0200

    fixed loop

--- philo/sources/routine.c
+++ philo/sources/routine.c
@@ -6,12 +6,13 @@
 /*   By: fpurdom <fpurdom@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/26 14:04:21 by fpurdom       #+#    #+#                 */
-/*   Updated: 2022/05/31 19:56:00 by fpurdom       ########   odam.nl         */
+/*   Updated: 2022/05/31 20:03:38 by fpurdom       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/actions.h"
 #include "include/timing_utils.h"
+#include <unistd.h>
 
 void	*routine(void *void_philo)
 {
@@ -19,7 +20,7 @@ void	*routine(void *void_philo)
 
 	philo = (t_philo *)void_philo;
 	while (!philo->var->start_time)
-		;
+		usleep(200);
 	if (!(philo->i % 2))
 		ft_delay(2000, philo->var);
 	if (philo->cycles >= 0)
